c2c21b626fcf381a3407367ca895870212742c25
Joe Desbonnet <jdesbonnet@gmail.com>
2017-01-26 11:49:00 +0000

    fix nuig weather script

--- scripts/download_nuig_weather.sh
+++ scripts/download_nuig_weather.sh
@@ -5,7 +5,7 @@ DBHOST=danu2.it.nuigalway.ie
 DBNAME=mydb586
 START_TIME="20161205"
 WEATHER_COLUMNS="DateTime,Temperature,RelativeHumidity,WindSpeed,WindDirection,Gust,BarometricPressure,TotalSolar,DiffuseSolar,BatteryOfWeatherStation"
-mysql --user=$DBUSER --password=$DBPW --host=$DBHOST $DBNAME -e "SELECT DateTime,Rainfall FROM hour where DateTime > $START_TIME ORDER BY DateTime"  | java ConvertTimeFormat - > ../data/nuig_preciptation.dat
+mysql --user=$DBUSER --password=$DBPW --host=$DBHOST $DBNAME -e "SELECT DateTime,Rainfall FROM hour where DateTime > $START_TIME ORDER BY DateTime"  | java ConvertTimeFormat - > ../data/nuig_precipitation.dat
 mysql --user=$DBUSER --password=$DBPW --host=$DBHOST $DBNAME -e "SELECT $WEATHER_COLUMNS FROM minute where DateTime > $START_TIME ORDER BY DateTime"  | java ConvertTimeFormat - > ../data/nuig_weather.dat
 
 
